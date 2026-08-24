1ad58707c6da7d3814333d58c39331189cddafd5
Lakshan <lakshanjayalath171@gmail.com>
2026-08-24 15:10:18 +0530

    Created all UI of hero section

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,7 +1,19 @@
 const Hero = () => {
   return (
-    <div>
-        <video src="/video/hero.mp4" autoPlay loop muted className='w-screen h-screen object-cover' />
+    <div className='relative h-screen w-full flex items-center justify-center'>
+        <video src="/Videos/hero.mp4" autoPlay muted className=' w-full h-full object-cover z-[-1]' />
+
+        <div className="absolute w-screen  bottom-24 left-2 px-10 p-2 flex items-center justify-between">
+          <div>
+            <h3 className="text-4xl font-bold">AirPods Max 2</h3>
+            <h1 className="text-8xl font-bold">Listening.<br/> Remastered.</h1>
+          </div>
+
+          <div className="flex apple rounded-full items-center justify-center px-6 py-4 gap-3">
+            <h3 className="text-black text-2xl">549 $</h3>
+            <button className="bg-blue-500  rounded-full px-4 py-1 cursor-pointer">Buy</button>
+          </div>
+        </div>
     </div>
   )
 }
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between '>
@@ -8,8 +6,8 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center justify-center gap-4 pr-6">
-        <img src="/icons/search.svg" className="w-6 h-6 cursor-pointer" alt="search" />
-        <img src="/icons/cart.svg" className="w-6 h-6 cursor-pointer" alt="cart" />
+        <img src="/icons/search.svg" className="w-10 h-10 cursor-pointer" alt="search" />
+        <img src="/icons/cart.svg" className="w-10 h-10 cursor-pointer" alt="cart" />
       </div>
     </div>
   )
