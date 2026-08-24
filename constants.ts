0393f8beeecc718684731c0224399285b9c87da8
Lakshan <lakshanjayalath171@gmail.com>
2026-08-25 00:57:07 +0530

    Hero section successfully implemented

---
+++ constants.ts
@@ -0,0 +1,32 @@
+const imageslider = [
+    {
+        id: 1,
+        image: "/public/images/slider (1).jpg",
+        content: "Up to 1.5x more Active Noise Cancellation than the previous generation",
+        position: "absolute top-1/2 right-2",
+    },
+    {
+        id: 2,
+        image: "/public/images/slider (2).jpg",
+        content: "Improved high‑fidelity sound. Exhilarating audio with deep bass, expansive mids, and crisp highs.",
+        position: "absolute top-1/2 left-2",
+    },
+    {
+        id: 3,
+        image: "/public/images/slider (3).jpg",
+        content: "New intelligent features: Live Translation.2 Adaptive Audio.3 Conversation Awareness.3",
+        position: "absolute top-1/2 left-2",
+    },
+    {
+        id: 4,
+        image: "/public/images/slider (4).jpg",
+        content: "Over‑ear design. From canopy to cushion, built for unequaled fit and comfort — in five vibrant colors.",
+        position: "absolute top-0 right-1/2",
+    },
+    {
+        id: 5,
+        image: "/public/images/slider (5).jpg",
+        content: "The ultimate over‑ear listening experience.",
+        position: "absolute top-1/2 right-2",
+    }
+]
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from "./component/Hero"
 import Navbar from "./component/Navbar"
+import Slider from "./component/Slider"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Slider/>
     </div>
   )
 }
